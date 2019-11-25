87ad42fb9e3a2b491e2835ad875a40156899e7b1
graybat21 <graybat21@gmail.com>
2019-11-25 17:32:15 +0900

    Fixed AB#5

--- selection/views.py
+++ selection/views.py
@@ -3,24 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
-from .models import Selection
+from .models import Selection, Participation, Choice
 
 
 def index(request):
     # /selection/ GET -> 전체 selection 목록 보여주기
-    # return HttpResponse("Hello, world. You're at the polls index.")
     latest_selection_list = Selection.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('selection/index.html')
     context = {
         'latest_selection_list': latest_selection_list,
     }
     # output = ', '.join([q.subject for q in latest_selection_list])
-    return HttpResponse(template.render(context, request))
+    return render(request, 'selection/index.html', context)
+
 
 def detail(request, selection_id):
     # /selection/{selection_id} GET -> 특정 selection 정보 보여주기
-    return HttpResponse("You're looking at question %s." % selection_id)
+    response = "You're looking at selection_id %s." % selection_id
+    choice_list = Choice.objects.filter(selection_id=selection_id).order_by('-pub_date')
+    context = {
+        'choice_list': choice_list,
+    }
+
+    return render(request, 'selection/detail.html', context)
 
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
